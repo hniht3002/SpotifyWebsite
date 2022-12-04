03697a28cda9fc10b15fb7d9b87fe0530f92d0ba
hniht3002 <s2okaza2s@gmail.com>
2022-12-04 21:57:40 +0700

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,36 @@
+const api = [
+  {
+    heading: "Focus",
+    items: [
+      {
+        image: "./assets/img/focus/img1.jfif",
+        title: "Peaceful Piano",
+        desc: "Relax and indulge with beautiful piano pieces",
+      },
+      {
+        image: "./assets/img/focus/img2.jfif",
+        title: "Deep Focus",
+        desc: "Keep calm and focus with ambient and post-rock music.",
+      },
+      {
+        image: "./assets/img/focus/img3.jfif",
+        title: "Instrumental Study",
+        desc: "Focus with soft study music in the background.",
+      },
+      {
+        image: "./assets/img/focus/img4.jfif",
+        title: "chill lofi study beats",
+        desc: "The perfect study beats. Find your focus, crush your productivity.",
+      },
+      {
+        image: "./assets/img/focus/img5.jfif",
+        title: "Coding Mode",
+        desc: "Dedicated to all the programmers out there.",
+      },
+    ],
+  },
+];
+
+const contentContainer = document.querySelector(".content .container");
+contentContainer.insertAdjacentHTML("beforeend", "<h1>Hello1</h1>");
+contentContainer.insertAdjacentHTML("beforeend", "<h1>Hello2</h1>");
